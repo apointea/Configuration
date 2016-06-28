02bcf9f308d1834d8f536ea2ba22b8b9328ce0c4
kalif <kalif@Albator>
2016-06-28 23:22:59 +0200

    add ramdisk utils

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #! /bin/bash
 
 # COLORS
-RED="\033[31m"
-GREEN="\033[32m"
-YELLOW="\033[33m"
-BLUE="\033[34m"
-MAGENTA="\033[35m"
-CYAN="\033[36m"
-CLEAR="\033[0m\n"
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+BLUE="\033[0;34m"
+MAGENTA="\033[0;35m"
+CYAN="\033[0;36m"
+BRED="\033[1;31m"
+BGREEN="\033[1;32m"
+BYELLOW="\033[1;33m"
+BBLUE="\033[1;34m"
+BMAGENTA="\033[1;35m"
+BCYAN="\033[1;36m"
+CLEAR="\033[0m"
 
 # SCRIPT DIR
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
