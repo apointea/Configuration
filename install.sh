295ee26dd325b2b391b297b43d50f4fcb1f6de42
apointea <web.pointeau@gmail.com>
2017-05-18 00:26:33 +0200

    update simplify

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #! /usr/bin/env bash
 
+# @Author: Antoine Pointeau <kalif>
+# @Date:   2016-09-19T00:46:29+02:00
+# @Email:  web.pointeau@gmail.com
+# @Filename: install.sh
+# @Last modified by:   kalif
+# @Last modified time: 2017-05-18T00:23:45+02:00
+
 # COLORS
 RED="\033[0;31m"
 GREEN="\033[0;32m"
@@ -13,72 +20,66 @@ BYELLOW="\033[1;33m"
 BBLUE="\033[1;34m"
 BMAGENTA="\033[1;35m"
 BCYAN="\033[1;36m"
-CLEAR="\033[0m"
+CLEAR="\033[0m\n"
 
 # SCRIPT DIR
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# IGNORE MOD
-IGNORE=false
-if [[ $# == 1 && ($1 == "-i" || $2 == "-i") ]]; then
-	IGNORE=true
+
+# REJECT ALL NON-LINUX
+OS="`uname`"
+if [[ $OS != "Linux" ]]; then
+    printf "$RED[-] Configuration only for Linux users$CLEAR"
+    exit 1
 fi
 
-printf "\n$MAGENTA----- OS specific install -----$CLEAR"
-OS="`uname`"
-printf "\n$GREEN[+] Os detected : $OS$CLEAR"
-case $OS in
-  'Linux') #LINUX
-	source $DIR/$OS/install.sh
-	;;
-  'Darwin') #MAC
-	source $DIR/$OS/install.sh
-	;;
-  *)
-	printf  "$YELLOW[!] no specific configuration for this os$CLEAR"
-	;;
+
+# INSTALL `lsb_release` IF NOT BY DEFAULT
+sudo yum install redhat-lsb -y > /dev/null 2>&1
+sudo apt-get install lsb-release -y > /dev/null 2>&1
+
+
+# CONFIGURE VARS RELATIVE TO THE DISTRIBUTION
+DISTRIB="`lsb_release -is`"
+case $DISTRIB in
+    'Debian')
+        PKG='apt-get'
+        ;;
+    'CentOS')
+        PKG='yum'
+        ;;
+    *)
+        printf  "$RED[-] No package manager pre-set for this distrib$CLEAR"
+        exit 1
+        ;;
 esac
 
-printf "\n$MAGENTA----- General install -----$CLEAR"
 
-printf "\n$BLUE[*] Install oh-my-zsh$CLEAR"
-if [[ ! -d $HOME/.oh-my-zsh || $IGNORE == true ]]; then
-	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-else
-	printf "$YELLOW[!] WARNING oh-my-zsh already installed$CLEAR"
-fi
+# INSTALL 'system-refresh.sh'
+printf "$BLUE[*] Install system auto refresh$CLEAR"
+sudo cp $DIR/scripts/system-refresh.sh /sbin/system-refresh.sh
+sudo echo "0 4 * * 7 root /sbin/system-refresh.sh $PKG" >> /etc/cron.d/system-refresh
+printf "$GREEN[+] DONE$CLEAR"
 
-printf "\n$BLUE[*] Add custom.zsh-theme$CLEAR"
-if [[ -d $HOME/.oh-my-zsh/themes/ || $IGNORE == true ]]; then
-	cp $DIR/custom.zsh-theme $HOME/.oh-my-zsh/themes/
-	printf "$GREEN[+] success $CLEAR"
-else
-	printf "$RED[-] ERROR no ~/.oh-my-zsh/themes/ directory $CLEAR"
-fi
 
-printf "\n$BLUE[*] Replace ~/.zshrc$CLEAR"
-cp $DIR/zshrc $HOME/.zshrc
+# REFRESH SYSTEM
+printf "$BLUE[*] Refresh system packages$CLEAR"
+sudo /sbin/system-refresh.sh $PKG
+printf "$GREEN[+] DONE$CLEAR"
 
-printf "\n$BLUE[*] Replace ~/.shell.sh with $OS version$CLEAR"
-rm $HOME/.shell.sh
-cat $DIR/shell.sh > $HOME/.shell.sh
-cat $DIR/$OS/shell.sh >> $HOME/.shell.sh
+# INSTALL COMMON PACKAGES
+COMMONS="vim wget zsh curl unzip"
+printf "$BLUE[*] Install common packages$CLEAR"
+sudo $PKG install -y $COMMONS
+printf "$GREEN[+] DONE$CLEAR"
 
-printf "\n$BLUE[*] Add bin directory to home$CLEAR"
-if [ ! -d $HOME/bin ]; then
-	cp -r $DIR/bin $HOME/bin
-	printf "$GREEN[+] success $CLEAR"
-	printf "$BLUE[*] copy OS specific bin$CLEAR"
-	cp -r $DIR/$OS/bin/* $HOME/bin
-elif [ $IGNORE == true ]; then
-	rm -rf $HOME/bin
-	cp -r $DIR/bin $HOME/bin
-	printf "$GREEN[+] success $CLEAR"
-	printf "$BLUE[*] copy OS specific bin$CLEAR"
-	cp -r $DIR/$OS/bin/* $HOME/bin
-else
-	printf "$YELLOW[!] WARNING ~/bin directory already exist$CLEAR"
-fi
-
-printf "\n$BLUE[*] Set zsh as default shell$CLEAR"
+# INSTALL oh-my-zsh
+printf "$BLUE[*] Install oh-my-zsh$CLEAR"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+printf "$GREEN[+] DONE$CLEAR"
+printf "$BLUE[*] Add custom oh-my-zsh theme$CLEAR"
+cp $DIR/res/custom.zsh-theme $HOME/.oh-my-zsh/themes/
+printf "$BLUE[*] Replace zshrc$CLEAR"
+cp $DIR/res/zshrc $HOME/.zshrc
+printf "$BLUE[*] Replace default shell$CLEAR"
 chsh -s `which zsh`
--- zshrc
+++
@@ -1,5 +0,0 @@
- export ZSH=$HOME/.oh-my-zsh
-ZSH_THEME="custom"
-plugins=(git)
-source $ZSH/oh-my-zsh.sh
-source ~/.shell.sh
